83750f3671e877a0edc5df2cf9608e49875e584e
forhye <4_hye@naver.com>
2024-04-12 18:08:39 +0900

    스위치문 추가

--- src/pages/Exam.jsx
+++ src/pages/Exam.jsx
@@ -1,76 +1,95 @@
-import './Exam.css'
+import "./Exam.css";
 
-import { useState } from 'react'
+import { useState } from "react";
 
 const Exam = () => {
+  const [calData, setCalData] = useState([]);
+  const [result, setResult] = useState("");
+  const [selectOption, setSelectoption] = useState("");
 
-    const [calData, setCalData] = useState([]);
-    const [result, setResult] = useState("");
-    const [selectOption, setSelectoption] = useState("");
+  const Left_Change = (e) => {
+    const L_value = e.target.value;
 
-    const Left_Change = (e) => {
-        const L_value = e.target.value
+    setCalData({
+      ...calData,
+      L_value,
+    });
+  };
 
-        setCalData({
-            ...calData,
-            L_value
-        })
+  const Right_Change = (e) => {
+    const R_value = e.target.value;
+
+    setCalData({
+      ...calData,
+      R_value,
+    });
+  };
+  console.log(calData);
+
+  const selectChange = (e) => {
+    setSelectoption(e.target.value);
+  };
+
+  const onClick = () => {
+    if (Number(calData["R_value"]) === 0) {
+      return setResult("0으로 나눌수 없습니다.");
     }
 
-    const Right_Change = (e) => {
-        const R_value = e.target.value
-
-        setCalData({
-            ...calData,
-            R_value
-        })
-    }
-    console.log(calData)
-
-    const selectChange = (e) => {
-        setSelectoption(e.target.value)
+    if (selectOption === "plus") {
+      setResult(Number(calData["L_value"]) + Number(calData["R_value"]));
+    } else if (selectOption === "minus") {
+      setResult(Number(calData["L_value"]) - Number(calData["R_value"]));
+    } else if (selectOption === "multiply") {
+      setResult(Number(calData["L_value"]) * Number(calData["R_value"]));
+    } else if (
+      selectOption === "divide" // && "R_value" !== "L_value"
+    ) {
+      setResult(Number(calData["L_value"]) / Number(calData["R_value"]));
     }
 
-    const onClick = () => {
-        if (selectOption === "plus") {
-            setResult(Number(calData["L_value"]) + Number(calData["R_value"]))
-        } else if (
-            selectOption === "minus"
-        ) {
-            setResult(Number(calData["L_value"]) - Number(calData["R_value"]))
-        } else if (
-            selectOption === "multiply"
-        ) {
-            setResult(Number(calData["L_value"]) * Number(calData["R_value"]))
-        } else if (
-            selectOption === "divide" // && "R_value" !== "L_value"
-        ) {
-            setResult(Number(calData["L_value"]) / Number(calData["R_value"]))
-        }
+    // 스위치 문으로 표현
+    // switch (selectOption) {
+    //   case "plus":
+    //     return setResult(
+    //       Number(calData["L_value"]) + Number(calData["R_value"])
+    //     );
+    //   case "minus":
+    //     return setResult(
+    //       Number(calData["L_value"]) - Number(calData["R_value"])
+    //     );
+    //   case "multiply":
+    //     return setResult(
+    //       Number(calData["L_value"]) * Number(calData["R_value"])
+    //     );
+    //   case "divide":
+    //     return setResult(
+    //       Number(calData["L_value"]) / Number(calData["R_value"])
+    //     );
+    //   default:
+    //     return "올바른 값을 입력해주세요";
+    // }
+  };
 
-    }
+  return (
+    <div className="Cal">
+      <div>
+        <input type="text" name="left_value" onChange={Left_Change} />
+      </div>
 
+      <select onChange={selectChange}>
+        <option value="plus">+</option>
+        <option value="minus">-</option>
+        <option value="multiply">*</option>
+        <option value="divide">/</option>
+      </select>
 
-    return (
-        <div className="Cal">
-            <div>
-                <input type="text" name="left_value" onChange={Left_Change} />
-            </div>
+      <input type="text" name="right_value" onChange={Right_Change} />
 
-            <select onChange={selectChange}>
-                <option value="plus">+</option>
-                <option value="minus">-</option>
-                <option value="multiply">*</option>
-                <option value="divide">/</option>
-            </select>
+      <button onClick={onClick}>=</button>
 
-            <input type="text" name="right_value" onChange={Right_Change} />
-
-            <button onClick={onClick}>=</button>
-
-            <div>{result}</div>
-        </div >
-    )
-}
+      <div>{result}</div>
+    </div>
+  );
+};
 
 export default Exam;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,52 +1,55 @@
-import './Home.css'
-import Button from '../components/Button';
-import BoardList from '../components/BoardList';
+import "./Home.css";
+import Button from "../components/Button";
+import BoardList from "../components/BoardList";
 
-import { useNavigate } from "react-router-dom"
+import { useNavigate } from "react-router-dom";
 
 const Home = ({ data }) => {
+  const navigate = useNavigate();
 
-    const navigate = useNavigate();
+  const handleCreate = (path) => {
+    navigate(path);
+  };
 
-    const handleCreate = (path) => {
-        navigate(path);
-    }
+  return (
+    <div className="Home">
+      <h1 className="home_title">게시판 제목</h1>
 
-
-    return (
-        <div className="Home">
-            <h1 className="home_title">게시판 제목</h1>
-
-            <div className="action_tab">
-                <div className="sort">
-                    <select name="" id="">
-                        <option selected>정렬</option>
-                        <option>최신순</option>
-                        <option>오래된순</option>
-                    </select>
-                </div>
-                <div className="create">
-                    <Button
-                        type="PRIMARY"
-                        text="작성하기"
-                        onClick={() => handleCreate('/create')} />
-                </div>
-            </div>
-
-            <div className="list">
-                <BoardList data={data} />
-            </div>
-
-            <div>페이징</div>
-
-            <div className="search">
-                <input className="input" type="text" placeholder='검색어를 입력하세요'></input>
-                <div className="button">
-                    <Button type="SEARCH" text="검색" />
-                </div>
-            </div>
+      <div className="action_tab">
+        <div className="sort">
+          <select name="" id="">
+            <option selected>정렬</option>
+            <option>최신순</option>
+            <option>오래된순</option>
+          </select>
         </div>
-    )
-}
+        <div className="create">
+          <Button
+            type="PRIMARY"
+            text="작성하기"
+            onClick={() => handleCreate("/create")}
+          />
+        </div>
+      </div>
+
+      <div className="list">
+        <BoardList data={data} />
+      </div>
+
+      <div>페이징</div>
+
+      <div className="search">
+        <input
+          className="input"
+          type="text"
+          placeholder="검색어를 입력하세요"
+        ></input>
+        <div className="button">
+          <Button type="SEARCH" text="검색" />
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
